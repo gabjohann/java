091ee76a4f5666bce8e40575ab3133e20511d6a2
Lucas Johann <aluno@MacBook-Pro-de-Administrador-3.local>
2023-09-12 15:16:17 -0300

    add: close scanner and comments

--- aula00/exercicio02/src/Main.java
+++ aula00/exercicio02/src/Main.java
@@ -14,5 +14,13 @@ public class Main {
         int num2 = scanner.nextInt();
 
         System.out.print("O primeiro número digitado foi: "  + num1 + " e o segundo foi: " + num2);
+
+        scanner.close();
+
+        /*
+         Declarando os valores como int, limita-se a crer que o usuário irá digitar valores interiors
+         o ideal seria declarar como double e inicializar as variávies no início do código
+         */
+
     }
 }
