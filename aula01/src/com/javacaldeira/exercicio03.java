7687cceb2610e2a6ad1411f043f1ccee5ede57a6
Lucas Johann <aluno@MacBook-Pro-de-Administrador-3.local>
2023-09-14 15:55:30 -0300

    add: exercise 03

--- aula01/src/com/javacaldeira/exercicio03.java
+++ aula01/src/com/javacaldeira/exercicio03.java
@@ -1,2 +1,22 @@
-package com.javacaldeira;public class exercicio03 {
+package com.javacaldeira;
+
+import java.util.Scanner;
+
+/*
+    Escreva um programa que separa a string em duas metades e imprime a primeira parte.
+*/
+public class exercicio03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String userInput;
+
+        System.out.print("Digite um palavra ou frase: ");
+        userInput = scanner.nextLine();
+        userInput = userInput.replaceAll("\\s", "");
+
+        int splitString = userInput.length() / 2;
+        System.out.println(userInput.substring(0, splitString));
+
+        scanner.close();
+    }
 }
