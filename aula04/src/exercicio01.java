608768c028c6d1745975ac5e3e0719155654a041
Lucas Gabriel <gabif.johann@gmail.com>
2023-09-26 19:25:06 -0300

    docs: exercise statement

---
+++ aula04/src/exercicio01.java
@@ -0,0 +1,25 @@
+/*
+    Descrição: Crie um programa Java que peça ao usuário para inserir 5 números inteiros e armazene-os em um array.
+    Em seguida, calcule e exiba a soma e a média dos números inseridos.
+*/
+import java.util.Scanner;
+
+public class exercicio01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int total = 0;
+        double avg = 0;
+
+        int[] numbersList = new int[5];
+        System.out.print("Digite 5 números inteiros separados por espaço: ");
+
+        for (int i = 0; i < 5; i++) {
+            numbersList[i] = scanner.nextInt();
+            total += numbersList[i];
+
+            avg = (double) total / 5;
+        }
+
+        System.out.println("A soma dos valores inseridos é: " + total + " e a média é: " + avg);
+    }
+}
