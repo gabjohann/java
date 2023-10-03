4b447290abd003009681dd3d6251a9ba929bc645
Lucas Johann <aluno@MacBook-Pro-de-Administrador-3.local>
2023-10-03 15:59:09 -0300

    add: exercicio01

---
+++ src/exercicio01/Circulo.java
@@ -0,0 +1,8 @@
+package exercicio01;
+
+public class Circulo implements Desenhavel {
+    @Override
+    public void draw() {
+        System.out.println("Desenhe um círculo.");
+    }
+}
---
+++ src/exercicio01/Main.java
@@ -0,0 +1,11 @@
+package exercicio01;
+
+public class Main {
+    public static void main(String[] args) {
+        Circulo drawCircle = new Circulo();
+        Quadrado drawSquare = new Quadrado();
+
+        drawCircle.draw();
+        drawSquare.draw();
+    }
+}
