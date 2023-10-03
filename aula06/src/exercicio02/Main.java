4ca115dce946e702c5a9a544b58cfaa2a865df77
Lucas Johann <aluno@MacBook-Pro-de-Administrador-3.local>
2023-10-03 16:01:59 -0300

    fix: add missing class folder

---
+++ aula06/src/exercicio02/Main.java
@@ -0,0 +1,14 @@
+package exercicio02;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String dayOfWeek;
+
+        System.out.print("Digite um dia da semana: ");
+        dayOfWeek = scanner.next().toUpperCase();
+    }
+}
---
+++ aula06/src/exercicio04/Calculavel.java
@@ -0,0 +1,8 @@
+package exercicio04;
+
+public interface Calculavel {
+    void addition();
+    void subtraction();
+    void multiplication();
+    void division();
+}
